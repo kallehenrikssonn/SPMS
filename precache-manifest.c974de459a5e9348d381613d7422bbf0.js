24559aed74b4d8bd24e6d29ca92c3714aaf45aac
Kalle Henriksson <kajuhe@utu.fi>
2019-12-03 13:38:26 +0200

    Updates

---
+++ precache-manifest.c974de459a5e9348d381613d7422bbf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e7526f549ef861b782c7808dcdd650",
+    "url": "/csdewas-project-2-kallehenrikssonn/index.html"
+  },
+  {
+    "revision": "fb508b9195fb127a8fdb",
+    "url": "/csdewas-project-2-kallehenrikssonn/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ff91266da9caf55b648f",
+    "url": "/csdewas-project-2-kallehenrikssonn/static/js/2.018416c1.chunk.js"
+  },
+  {
+    "revision": "fb508b9195fb127a8fdb",
+    "url": "/csdewas-project-2-kallehenrikssonn/static/js/main.061b2437.chunk.js"
+  },
+  {
+    "revision": "c24199190f115018dc5d",
+    "url": "/csdewas-project-2-kallehenrikssonn/static/js/runtime-main.daed946d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/csdewas-project-2-kallehenrikssonn/static/media/logo.25bf045c.svg"
+  }
+]);
